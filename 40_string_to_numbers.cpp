25ed03e6d398d115db0a43b6fd8b8fe421a24e07
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2024-03-15 09:16:34 +0530

    String to number using stoi

---
+++ 40_string_to_numbers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    string strNumber = "1012";
+    int number1 = stoi(strNumber);
+    // int number2 = atoi(strNumber);
+
+    // int index = size(strNumber) - 1;
+
+    // while (index >= 0){
+    //     char ch = strNumber.at(index);
+    //     // int digit = (int) ch;
+    //     int digit = ch.
+    //     number = number * 10 + digit;
+    //     index--;
+    // }
+    cout<<"Number formed is "<<number1<<endl;
+    // cout<<"Number formed is "<<number2<<endl;
+    return 0;
+}
